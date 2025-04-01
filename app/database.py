d43c9b06452f414b5460136c3be7f0a68bfee79a
Damián Tortosa <gcasual160@gmail.com>
2025-04-01 19:38:16 +0200

    ADD .env functional

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://admin:1234@db/eventos_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
